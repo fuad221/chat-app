02659aa8ad946284ccf25e434815673db3596a6f
fuad221 <fuad221>
2021-11-14 19:52:27 +0100

    I finished the file's MyMessage

--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
-function MyMessage() {
+function MyMessage({ message }) {
+    if (message.attachments && message.attachments.lenhgt > 0)
+        return (
+            <img
+                src={message.attachment[0].file}  // the file is location system for the file
+                alt="message-attachment"
+                className="message-image"
+                style={{ float: "right" }}
+
+            />
+        )
     return (
-        <div>
-            
+        <div className="message" style={{float: "right", marginRight: "18px", color: "white", backgroundColor: "#3B2A50"}} >
+
         </div>
     )
 }
